c6e46c0d129a226552f73d56d17e7021a4e3d3f1
Manpreet Babbra <babbram@macc02x29scjg5h.home>
2019-12-07 20:36:43 +0000

    Move to an org based init

--- init.el
+++ init.el
@@ -1,27 +1,12 @@
-(add-to-list 'load-path "~/.emacs.d/customizations")
-
-(load "packages.el")
-(load "theme.el")
-(load "navigation.el")  ;; from Clojure for the Brave 
-(load "ui.el")
-(load "configurations.el")
-(load "elisp-editing.el")
-;(load "modeline.el")    ;; doom mode line config
-(load "clojure-editing.el")
-(load "javascript-editing.el")
-(load "keyboard.el")
-
+(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "b35a14c7d94c1f411890d45edfb9dc1bd61c5becd5c326790b51df6ebf60f402" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" default)))
  '(package-selected-packages
    (quote
-    (htmlize json-mode flymake-json yasnippet fsharp-mode company-lsp lsp-mode doom-themes yaml-mode multiple-cursors magit tagedit rainbow-delimiters projectile smex ido-completing-read+ paredit cider))))
+    (clojure-snippets yasnippet yaml-mode which-key use-package try treemacs tagedit smex rainbow-mode rainbow-delimiters projectile paredit neotree multiple-cursors minions magit lsp-ui json-mode ido-completing-read+ htmlize fsharp-mode flymake-json exec-path-from-shell doom-themes doom-modeline counsel company-lsp clojure-mode-extra-font-locking cider auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
