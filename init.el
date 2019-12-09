332973ad8d98dd2fc58a5795ef3c7ab14cc22be7
flipmokid <manpreet_babbra@yahoo.co.uk>
2019-12-09 10:07:20 +0000

    Update init.el
    Delete auto-generated block.

--- init.el
+++ init.el
@@ -1,15 +1,1 @@
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (clojure-snippets yasnippet yaml-mode which-key use-package try treemacs tagedit smex rainbow-mode rainbow-delimiters projectile paredit neotree multiple-cursors minions magit lsp-ui json-mode ido-completing-read+ htmlize fsharp-mode flymake-json exec-path-from-shell doom-themes doom-modeline counsel company-lsp clojure-mode-extra-font-locking cider auto-complete))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
